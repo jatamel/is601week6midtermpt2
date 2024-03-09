1d1c298caf095921f4afc5b5f3ace503d88c5d86
jatamel <jm2449@njit.edu>
2024-03-09 11:54:37 -0500

    hashed the dotenv out

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import pkgutil
 import importlib
 import sys
 from app.commands import CommandHandler, Command
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import logging
 import logging.config
 
@@ -11,7 +11,7 @@ class App:
     def __init__(self):
         os.makedirs('logs', exist_ok=True)
         self.configure_logging()
-        load_dotenv()
+        #load_dotenv()
         self.settings = self.load_environment_variables()
         self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
         self.command_handler = CommandHandler()
